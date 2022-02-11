ef98e799f9d33a56a5e2c004db09f576363953d1
EC2 Default User <ec2-user@ip-172-31-35-132.us-east-2.compute.internal>
2022-02-11 07:30:09 +0000

    first commit

---
+++ Modules/ec2.tf
@@ -0,0 +1,94 @@
+provider "aws"{
+
+ region = "${var.region}"
+
+ #access_key = "**********"
+
+ #secret_key = "**********"
+
+ }
+
+resource "aws_instance" "ec2_pro" {
+
+ availability_zone = "${var.availability_zones}"
+
+ ami ="${var.ami}"
+
+ instance_type = "${var.instance_type}"
+
+ key_name = "Devops1"
+
+ #count = var.instance_count
+
+ tags = {
+
+ Name = "Node"
+
+ #Name = "Terraform-${count.index + 1}"
+
+ }
+
+ security_groups= ["${aws_security_group.Devops.name}"] 
+
+ }
+
+
+
+ resource "aws_security_group" "Devops" {
+
+ name="Devops"
+
+
+ingress {
+
+ from_port = 22
+
+ to_port = 22
+
+ protocol = "TCP"
+
+cidr_blocks = ["0.0.0.0/0"]
+
+ }
+
+ 
+ ingress {
+
+ from_port = 8080
+
+ to_port = 8080
+
+ protocol = "TCP"
+
+ cidr_blocks = ["0.0.0.0/0"]
+
+ }
+
+
+ egress {
+
+ from_port = 0
+
+ to_port = 0
+
+ protocol = "-1"
+
+ cidr_blocks = ["0.0.0.0/0"]
+
+
+ }
+
+}
+
+
+# resource "aws_ebs_volume" "volume1" {
+
+# availability_zone = "us-east-2c"
+
+# size = 1
+
+# encrypted = true
+
+ 
+
+# }
---
+++ layers/terraform.tf
@@ -0,0 +1,13 @@
+module "ec2" {
+
+  source = "../Modules"
+
+  region = "us-east-2"
+
+  availability_zone = "us-east-2c"
+
+  ami = "ami-0b614a5d911900a9b"
+
+  instance_type = "t2.micro"
+
+  }
